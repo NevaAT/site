c7dac6debbd95ddcc5079f223f263a7d87918b7d
VladimirAmosov <89339811+NevaAT@users.noreply.github.com>
2021-08-22 19:54:53 +0300

    joined backend

--- components/BrandSlider.js
+++ components/BrandSlider.js
@@ -1,23 +1,30 @@
 import { useEffect, useState  } from 'react'
-import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Navigation, Autoplay } from 'swiper'
+import { API_URL } from '@/config/index'
 
-const Brands = require('../data/brands.json')
+// const Brands = require('../data/brands.json')
 
 const BrandSlider = props => {
   
   const [ isMobile, setIsMobile ] = useState(false)
+  const [ brands, setBrands ] = useState(null)
+
+  const getBrands = async () => {
+    const res = await fetch(`${API_URL}/brands`)
+    setBrands(await res.json())
+  }
 
   useEffect(() => {
     const checkMobile = () => setIsMobile(window.innerWidth < 640)
     window.onresize = checkMobile
     checkMobile()
+    getBrands()
   }, [isMobile])
 
   SwiperCore.use([Navigation, Autoplay])
 
-  return (
+  return ( brands &&
     <Swiper
       spaceBetween={30}
       slidesPerView={isMobile? 2: 3}
@@ -28,9 +35,9 @@ const BrandSlider = props => {
       navigation
     >
       {
-        Brands.map((slide, i) => {
-          if (props.kind === slide.kind) return <SwiperSlide key={`slide_${slide.brand}_${i}`}>
-            <Image src={`/images/brands/brand-${slide.brand}.png`} alt={slide.brand} width={240} height={100} />
+        brands.map(slide => {
+          if (props.kind === slide.type) return <SwiperSlide key={`slide_${slide.id}`}>
+            <img src={`${slide.img.url}`} alt={slide.title} width={240} height={100} />
           </SwiperSlide>
         })
       }
