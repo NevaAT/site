99164b81700f569300e5fe9a658936e310c50f02
VladimirAmosov <89339811+NevaAT@users.noreply.github.com>
2022-06-22 19:14:58 +0300

    updated dependances

--- components/InnerText.js
+++ components/InnerText.js
@@ -1,9 +1,9 @@
-import marked from 'marked'
+import { marked } from 'marked'
 
 const InnerText = ({menus, item}) => <div className="px-2 mb-12 text-center text-bluegray-300"
 dangerouslySetInnerHTML={{
   __html: menus.find(menu => menu.Ref === item) && 
-    marked(menus.find(menu => menu.Ref === item).Text) || "-- empty --"
+    marked.parse(menus.find(menu => menu.Ref === item).Text) || "-- empty --"
 }}>
 </div>
 
--- components/TextChapter.js
+++ components/TextChapter.js
@@ -1,5 +1,5 @@
-import marked from 'marked'
+import { marked } from 'marked'
 
-const Chapter = ({text}) => <div dangerouslySetInnerHTML={{__html: text && marked(text) || '--empty--'}}></div>
+const Chapter = ({text}) => <div dangerouslySetInnerHTML={{__html: text && marked.parse(text) || '--empty--'}}></div>
 
 export default Chapter
